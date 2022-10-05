c93f4d1396568bee152757caac5961f73bfde29c
vamsiadmin <vamsiadmin@vsstudio>
2022-10-05 09:13:12 +0000

    Add project files.

---
+++ WebApp/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string EmpName { get; set; }
+        public string DeptName  { get; set; }
+
+    }
+}
---
+++ WebappDatabase/dbo/Tables/Employee.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [EmpName]  NVARCHAR (MAX) NOT NULL,
+    [DeptName] NVARCHAR (MAX) NOT NULL,
+    [Email] NVARCHAR(50) NULL, 
+    CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
